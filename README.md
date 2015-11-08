44dfcfea47f89296c73da5ec70bcae9ca923bb76
Akshat Pandey <akshat0123@gmail.com>
2015-11-08 11:37:00 -0500

    adjusted multithreading, added prettier output

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ Bear in mind, the script is written in a single-thread, and there are around
 haven't downloaded all pages of links worth, but I'd estimate around 100 hrs at
 least, based on how long it takes for 1-5 pages 0f 250 links each), and you're
 probably going to need around 7-8 gigabytes (give or take a hundred or so
-megabytes) of room.
+megabytes) of room. Running the program will also create two additional files,
+'bills.csv', and 'records.txt', which contain some information about the bills
+being downloaded.
 
 ##Dependencies
 Running this file requires Python 2.7 and the python library BeautifulSoup4. On
@@ -19,14 +21,15 @@ respectively.
 
 ##Configuration
 Clone the repository, and edit the `BILL_DIRECTORY` field to specify the file
-where you would like the bill text files to be placed. 
+where you would like the bill text files to be placed. also make sure to edit
+the `thread_count` field to set the number of threads to be run at once. I've
+had the best results with using 10.
 
 ##Notes
-This is a multi-threaded program that runs 10 threads in order to improve I/O
-performance. The congress.gov website robots.txt specifies a crawl-delay of 2
-seconds. This application places at least a 2-second delay between the creation
-of each 10 threads. It may not be a good idea to download all the pages of
-legislation at once - either due to getting your ip blocked, or just because,
-despite the fact that this is multi-threaded (at least multithreading as far as
-python supports multithreading... look up python multithreading gil if you want
-to know more about that), it will still take a looong time to run.
+This is a multi-threaded program that runs multiple threads in order to improve
+I/O performance. The congress.gov website robots.txt specifies a crawl-delay of
+2 seconds. It may not be a good idea to download all the pages of legislation at
+once - either due to getting your ip blocked, or just because, despite the fact
+that this is multi-threaded (at least multithreading as far as python supports
+multithreading... look up python multithreading if you want to know more
+about that), it will still take a looong time to run.
--- get_bills.py
+++ get_bills.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup 
-import urllib2, threading, sys, time
+import urllib2, threading, sys, time, os
+
+def gcd(x,y) :
+    while y : x,y = y,x%y
+    return x
+
+def lcm(x,y) : return (x*y)/gcd(x,y)
 
 def get_soup(url) :
     try : 
@@ -25,63 +31,72 @@ def get_bill_text(url) :
     if text != None : text = text.get_text().encode('utf-8')
     return text
 
-def process_bill(bill, bills) :
+def process_bill(bill, bills, path) :
     bill_name, bill_address = get_bill_info(bill)
-    bills.write("%s,%s," % (bill_name, bill_address))
+    page = path.split('page_',1)[1]
 
     bill_text = get_bill_text(bill_address)
 
     if bill_text != None :
-        current_bill = open(BILL_DIRECTORY + bill_name + '.txt','w')
+        current_bill = open(path + bill_name + '.txt','w')
         current_bill.write(bill_text)
-        bills.write("%s,%s,%s\n" %(bill_name, bill_address,(BILL_DIRECTORY+bill_name+'.txt')))
+        bills.write("%s,%s,%s,%s\n" %(page, bill_name, bill_address,(BILL_DIRECTORY+bill_name+'.txt')))
     else : 
-        bills.write("%s,%s,%s\n" %(bill_name, bill_address,'NO TEXT AVAILABLE'))
+        bills.write("%s,%s,%s,%s\n" %(page, bill_name, bill_address,'NO TEXT AVAILABLE'))
 
 if __name__ == "__main__":
 
     sys.setrecursionlimit(2000)
 
+    # BILL_DIRECTORY : the directory the bill texts will be downloaded to 
     BASE_URL       = "https://www.congress.gov"
     BASE_SEARCH    = "https://www.congress.gov/legislation?pageSize=250&page="
     BILL_DIRECTORY = "/YOUR BILL DIRECTORY HERE/"
     START_TIME     = time.time()
 
+    # THREAD COUNT : the number of threads to use 
+    start_page   = 0
     number_pages = get_number_of_pages(BASE_SEARCH + '1')
-    bills = open('bills.csv','w')
+    thread_count = 10 
+    thread_lcm   = lcm(thread_count,25)
+    hash_num     = thread_lcm/25
+    bills        = open('bills.csv','a')
+    records      = open('records.txt','a')
 
     # OUTPUT
     sys.stdout.write('\nRetrieving legislation...\n\n')
 
-    for i in range(number_pages) :
+    for i in range(start_page, number_pages) :
 
         progress_bar = '\r    page %d of %d: ' % (i+1, number_pages)
         sys.stdout.write(progress_bar)
 
-        soup        = get_soup(BASE_SEARCH + str(i+1))
-        dirty_bills = soup.find('ol', {'class':'results_list'}).findAll('h2')
-        bill_count  = 0
+        soup           = get_soup(BASE_SEARCH + str(i+1))
+        dirty_bills    = soup.find('ol', {'class':'results_list'}).findAll('h2')
+        bill_count     = 0
+        page_directory = BILL_DIRECTORY + 'page_' + str(i+1) + '/'
+        page_start     = time.time()
+        os.makedirs(page_directory)
 
-        for b1,b2,b3,b4,b5,b6,b7,b8,b9,b10 in (dirty_bills[i:i+10] for i in range (0,250,10)) :
+        for j in range(0,250,thread_count) :
 
-            dbs = [b1,b2,b3,b4,b5,b6,b7,b8,b9,b10]
             threads = []
-
-            bill_count += 10
-            if bill_count % 50 == 0 :
-                progress_bar += '##' 
+            bill_count += thread_count 
+            if bill_count % thread_lcm == 0 :
+                progress_bar += ('#'*hash_num)
                 sys.stdout.write(progress_bar)
                 sys.stdout.flush()
 
-            delay_start = time.time()
-            for db in dbs :
-                thread = threading.Thread(target=process_bill, args=(db,bills))
-                threads.append(thread) 
+            for k in range(thread_count) :
+                thread = threading.Thread(target=process_bill,\
+                                          args=(dirty_bills[j+k],bills,page_directory))
+                threads.append(thread)
                 thread.start()
 
             for thread in threads : thread.join()
 
-            if (time.time() - delay_start) < 2.0 : time.sleep(2)
+        records.write("Page %d, time: %s\n" % (i, (time.time() - page_start)))
+        sys.stdout.write('\n')
 
     sys.stdout.write('\nDone!\n')
     sys.stdout.write('Retrieval took %s seconds\n' % (time.time() - START_TIME))
